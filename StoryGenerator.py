13cad2e1ed5c2312aabd0cd1b715fc1d01d1056d
Abdulmajeed Masrahi <111733998+MJEEd91@users.noreply.github.com>
2022-10-29 15:11:41 +0300

    second commit

--- StoryGenerator.py
+++ StoryGenerator.py
@@ -1,3 +1,4 @@
+import time
 import random
 when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago', 'On 20th jan']
 who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle', 'a cat']
@@ -6,4 +7,6 @@ residence = ['Barcelona', 'India', 'Germany', 'Venice', 'England']
 went = ['cinema', 'university', 'seminar', 'school', 'laundry']
 happened = ['made a lot of friends', 'Eats a burger', 'found a secret key', 'solved a mistery']
 print(random.choice(when) + ', ' + random.choice(who) + ' that lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+
+time.sleep(4)
 print("\n\nEnd of story. ")
